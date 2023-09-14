1c6cbe04d2a65bea421d65442b0e2aa1ec7c1d11
Mathias Jensen <maje419@student.sdu.dk>
2023-09-14 14:14:04 +0200

    Added simple example

---
+++ SimpleExample/simple-producer/src/main/java/com/mycompany/app/KafkaRelayer.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+import jolie.runtime.JavaService;
+/**
+ * Hello world!
+ *
+ */
+public class KafkaRelayer extends JavaService 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
