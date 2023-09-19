805f9c8fbd53a831cddd562495f2d4fe50c42bfa
Mathias Jensen <maje419@student.sdu.dk>
2023-09-19 13:54:50 +0200

    Works

--- SimpleExample/simple-producer/src/main/java/com/mycompany/app/KafkaRelayer.java
+++ SimpleExample/simple-producer/src/main/java/com/mycompany/app/KafkaRelayer.java
@@ -1,11 +1,13 @@
 package com.mycompany.app;
 
 import java.io.IOException;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Properties;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-
 import jolie.runtime.JavaService;
 
 public class KafkaRelayer extends JavaService {
@@ -13,16 +15,23 @@ public class KafkaRelayer extends JavaService {
     private static String m_topicName = "local-demo";
 
     public static void main(String[] args) {
-        KafkaProducer<String, String> producer;
+        KafkaProducer<String, String> producer = null;
         try {
             Properties props = PropertiesHelper.getProperties();
-            producer = new KafkaProducer<>(props);
-            ProducerRecord<String, String> message = new ProducerRecord<>(m_topicName, "Test", "3");
-            producer.send(message);
-        } catch (IOException e) {
+            producer = new KafkaProducer<String, String>(props);
+            System.out.println("Hello World!");
+            while (true) {
+                ProducerRecord<String, String> message = new ProducerRecord<>(m_topicName, "Update",
+                        String.format("Test %s", LocalDateTime.now().toString()));
+                producer.send(message);
+                Thread.sleep(1000);
+            }
+        } catch (IOException | InterruptedException e) {
+            if (producer != null) {
+                producer.close();
+                System.out.println("Producer closed.");
+            }
             e.printStackTrace();
         }
-
-        System.out.println("Hello World!");
     }
 }
