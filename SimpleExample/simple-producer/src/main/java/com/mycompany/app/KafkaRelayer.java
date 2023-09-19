a6cd5941ee1bd5a8a05634ce6964e41cce0b8371
Mathias Jensen <maje419@student.sdu.dk>
2023-09-19 10:36:37 +0200

    wip

--- SimpleExample/simple-producer/src/main/java/com/mycompany/app/KafkaRelayer.java
+++ SimpleExample/simple-producer/src/main/java/com/mycompany/app/KafkaRelayer.java
@@ -1,13 +1,28 @@
 package com.mycompany.app;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
 import jolie.runtime.JavaService;
-/**
- * Hello world!
- *
- */
-public class KafkaRelayer extends JavaService 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class KafkaRelayer extends JavaService {
+
+    private static String m_topicName = "local-demo";
+
+    public static void main(String[] args) {
+        KafkaProducer<String, String> producer;
+        try {
+            Properties props = PropertiesHelper.getProperties();
+            producer = new KafkaProducer<>(props);
+            ProducerRecord<String, String> message = new ProducerRecord<>(m_topicName, "Test", "3");
+            producer.send(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Hello World!");
     }
 }
